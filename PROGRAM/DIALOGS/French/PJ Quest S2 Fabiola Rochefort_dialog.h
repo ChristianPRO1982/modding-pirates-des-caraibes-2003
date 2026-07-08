45f90edda1145b486b05ca488704cc5ffdd32cac
ChristianPRO1982 <christian.martiny@outlook.com>
2026-07-08 16:33:07 +0200

    start e13

---
+++ PROGRAM/DIALOGS/French/PJ Quest S2 Fabiola Rochefort_dialog.h
@@ -0,0 +1,10 @@
+string DLG_TEXT[8] = {
+"Bonjour capitaine.",
+"Bonjour. A qui ai-je l'honneur ?",
+"Je suis Fabiola Rochefort. Je vous observe depuis quelque temps deja.",
+"Je n'aime guere etre suivi, madame.",
+"Rassurez-vous. Je ne vous veux aucun mal. J'ai seulement vu combien de jeunes gens vous avez aides a se fiancer malgre leur maladresse et leur manque de fortune.",
+"Cela m'a surtout coute cher. J'espere qu'il y a une meilleure raison a cette rencontre.",
+"Justement. Il est temps de vous remercier. Prenez ce coffre de bijoux et faites-en bon usage, pour vous ou pour d'autres promesses a tenir.",
+"Merci, madame. Je saurai m'en souvenir."
+};
