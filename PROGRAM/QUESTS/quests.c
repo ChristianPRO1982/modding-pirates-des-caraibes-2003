03ee17204874bdfb05007ab3c8e8da361df40250
ChristianPRO1982 <christian.martiny@outlook.com>
2026-07-10 11:47:44 +0200

    .

--- PROGRAM/QUESTS/quests.c
+++ PROGRAM/QUESTS/quests.c
@@ -51,7 +51,7 @@ void QuestsCheck_forLocEnter()
 	// POUR CREER DES QUETES
 	SetNationRelation2MainCharacter(PIRATE, RELATION_FRIEND);
 	//PJ connaitre a chaque reload des informations (ex : position = pchar.location)
-	Log_SetStringToLog("PJ 1:"+pchar.location);
+	// Log_SetStringToLog("PJ 1:"+pchar.location);
 	// Log_SetStringToLog("PJ 2:"+pchar.location.locator);
 	// Log_SetStringToLog("PJ 3:"+pchar.location.locator_group);//location.group
 	// Log_SetStringToLog("PJ blade:"+pchar.equip.blade);
